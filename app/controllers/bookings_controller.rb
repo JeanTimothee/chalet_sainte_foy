139daae9fbb1e76c1d5683fdb0dc1fd1f2269377
Timothee Regis <registimothee@gmail.com>
2020-08-21 13:11:59 +0200

    added flatpickr

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,27 @@
 class BookingsController < ApplicationController
-
   def destroy
-    raise
+    @booking = Booking.find(params[:id])
+    @chalet = Chalet.find(@booking.chalet_id)
+    @booking.delete
+    redirect_to chalet_path(@chalet)
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.chalet = Chalet.find(params[:chalet_id])
+    if @booking.save
+      flash[:notice] = "Dates blocked!"
+      redirect_to chalet_path(@booking.chalet)
+    else
+      @chalet = @booking.chalet
+      @bookings = @chalet.bookings
+      render 'chalets/show'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :name)
   end
 end
