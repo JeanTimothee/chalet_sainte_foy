169e0c2a821916f6d3d2b8b21f44aba2e7e3cb7d
Timothee Regis <registimothee@gmail.com>
2020-08-26 18:27:08 +0200

    change @chalet finding method in controller#show

--- app/controllers/chalets_controller.rb
+++ app/controllers/chalets_controller.rb
@@ -1,7 +1,7 @@
 class ChaletsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
   def show
-    @chalet = Chalet.find(params[:id])
+    @chalet = Chalet.all.first
     @bookings = @chalet.bookings
     @booking = Booking.new
   end
