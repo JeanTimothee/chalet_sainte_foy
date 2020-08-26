5fe831c665b2bd689a8385dc2eeddeb651ce9070
Timothee Regis <registimothee@gmail.com>
2020-08-26 17:28:28 +0200

    remove index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :chalets, only: [:show] do
-    resources :bookings, only: [:index, :create]
+    resources :bookings, only: [:create]
   end
   resources :bookings, only: [:destroy]
 end
